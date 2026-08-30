0f3f6d8662831d8bedc7b4ecaef5b0f3693082a3
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-30 21:06:55 +0200

    Add problem statement comment to C5.2

--- ch05/C5.2.c
+++ ch05/C5.2.c
@@ -5,6 +5,19 @@
 //  Created by Aleksandar on 6/5/25.
 //
 
+/*******************************************************************
+ *  Chapter 5, Project 2                                           *
+ *                                                                 *
+ *  Reads a time in 24-hour format and prints the 12-hour          *
+ *  equivalent with an AM/PM suffix.                               *
+ *                                                                 *
+ *  Enter a 24-hour time: 21:11                                    *
+ *  Equivalent 12-hour time: 9:11 PM                               *
+ *                                                                 *
+ *  Note: midnight and noon must print as 12, not 0.               *
+ *******************************************************************/
+
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[])
