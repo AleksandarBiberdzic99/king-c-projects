f815a98a6252770049dc0022b5d596203f60fa57
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-30 23:26:21 +0200

    Add problem statement comment to C7.1

--- ch07/C7.1.c
+++ ch07/C7.1.c
@@ -5,7 +5,17 @@
 //  Created by Aleksandar on 7. 6. 2026..
 //
 
-/* Prints a table of squares using a for statement */
+/*******************************************************************
+ *  Chapter 7, Project 1                                           *
+ *                                                                 *
+ *  square2.c from Section 6.3 breaks down once i * i grows past   *
+ *  the largest value an int can hold. The task is to find the     *
+ *  smallest n where that happens, then repeat the experiment      *
+ *  with i declared as short and as long (adjusting the printf     *
+ *  conversion specifications each time) and draw a conclusion     *
+ *  about how many bits each integer type uses on this machine.    *
+ *******************************************************************/
+
 
 #include <stdio.h>
 
