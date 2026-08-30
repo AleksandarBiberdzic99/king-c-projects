b3430c58dec8a2421cf7b8ff4fb250c9a05e8c43
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-31 00:11:58 +0200

    Update C7.15g.c

--- ch07/C7.15g.c
+++ ch07/C7.15g.c
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[]) {
     long double factorial=1;
     
     
-    /* 7.15 d) Repeat part(a),using a float variable instead.
+    /* 7.15 g) Repeat part(a),using a long double variable instead.
      Long double on MacOS/ARM64 is implemented as 64 nit IEEE double,so tha maximum value of n that gives correct factoriel is identical to previous question for double 22.On Linux/x86/64 system long double is implemented as 80 bit extended format with mantisa 65 bits long and exoonent with 15 bits.Given that  2⁶⁴ ≈ 1,84 × 10¹⁹.  26!≈ 4,8 × 10¹⁹ is too large to fit,the next smallest value that fits is 25!≈ 3,7 × 10¹⁸ so the answer for largest n that long double gives correct factoriel on Linux/x86 sytem is 25.
          
      
