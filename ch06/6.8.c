a7aa8d801d1a72f82caf64ff28f2aff2a63453a1
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-30 22:35:28 +0200

    Add problem statement comment to C5.9

--- ch06/6.8.c
+++ ch06/6.8.c
@@ -5,6 +5,27 @@
 //  Created by Aleksandar on 18. 4. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 6, Project 8                                           *
+ *                                                                 *
+ *  Prints a one-month calendar. The user supplies the number of   *
+ *  days in the month and the weekday it starts on.                *
+ *                                                                 *
+ *  Enter number of days in month: 31                              *
+ *  Enter starting day of the week (1=Sun, 7=Sat): 3               *
+ *                                                                 *
+ *            1  2  3  4  5                                        *
+ *      6  7  8  9 10 11 12                                        *
+ *     13 14 15 16 17 18 19                                        *
+ *     20 21 22 23 24 25 26                                        *
+ *     27 28 29 30 31                                              *
+ *                                                                 *
+ *  A single for loop counts from 1 to n and prints each day; an   *
+ *  if inside it emits a newline whenever the day falls at the     *
+ *  end of a week.                                                 *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
