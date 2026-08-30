7b726a4f65dcb7bd788500aea8396cd295faf0ca
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-31 01:00:34 +0200

    Add problem statement comment to C8.13

--- ch08/C8.13.c
+++ ch08/C8.13.c
@@ -5,6 +5,20 @@
 //  Created by Aleksandar on 17. 6. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 8, Project 13                                          *
+ *                                                                 *
+ *  Extends Chapter 7, Project 11 so that the result is labelled.  *
+ *                                                                 *
+ *  Enter a first and last name: Lloyd Fosdick                     *
+ *  You entered the name: Fosdick, L.                              *
+ *                                                                 *
+ *  Only the last name has to be buffered in a character array     *
+ *  until the line can be printed; it is assumed to be at most     *
+ *  20 characters long.                                            *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
