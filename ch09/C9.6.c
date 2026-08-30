4a5b58ec027994e7f6c2ef42b5e68290debb0257
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-31 01:28:44 +0200

    C9.6

--- ch09/C9.6.c
+++ ch09/C9.6.c
@@ -5,6 +5,18 @@
 //  Created by Aleksandar on 1. 7. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 9, Project 6                                           *
+ *                                                                 *
+ *  Evaluates the polynomial                                       *
+ *                                                                 *
+ *      3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6                          *
+ *                                                                 *
+ *  in a separate function. The program reads a value for x,       *
+ *  calls the function, and prints the value it returns.           *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
