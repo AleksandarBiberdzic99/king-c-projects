f08e19c304fccde399658a10cbb04888fef9c8a8
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-30 23:33:51 +0200

    Update C7.6.c

--- ch07/C7.6.c
+++ ch07/C7.6.c
@@ -5,6 +5,14 @@
 //  Created by Aleksandar on 7. 6. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 7, Project 6                                           *
+ *                                                                 *
+ *  Prints the size in bytes of int, short, long, float, double    *
+ *  and long double on the current machine.                        *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
