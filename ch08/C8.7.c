453cb3c67ddb60772cafd7a9db221c580be706f8
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-31 00:49:09 +0200

    Add problem statement comment to C8.7

--- ch08/C8.7.c
+++ ch08/C8.7.c
@@ -5,6 +5,23 @@
 //  Created by Aleksandar on 14. 6. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 8, Project 7                                           *
+ *                                                                 *
+ *  Reads a 5 x 5 array of integers one row at a time, then        *
+ *  prints the sum of each row and the sum of each column.         *
+ *                                                                 *
+ *  Enter row 1: 8 3 9 0 10                                        *
+ *  Enter row 2: 3 5 17 1 1                                        *
+ *  Enter row 3: 2 8 6 23 1                                        *
+ *  Enter row 4: 15 7 3 2 9                                        *
+ *  Enter row 5: 6 14 2 6 0                                        *
+ *                                                                 *
+ *  Row totals: 30 27 40 36 28                                     *
+ *  Column totals: 34 37 37 32 21                                  *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
