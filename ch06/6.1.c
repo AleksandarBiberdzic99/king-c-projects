004d4cf7c559423b07f9eaaea39d92ab8470304c
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-30 22:22:14 +0200

    Add problem statement comment to C6.1

--- ch06/6.1.c
+++ ch06/6.1.c
@@ -5,11 +5,29 @@
 //  Created by Aleksandar on 15. 4. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 6, Project 1                                           *
+ *                                                                 *
+ *  Reads numbers one at a time until the user enters 0 or a       *
+ *  negative value, then prints the largest number entered.        *
+ *                                                                 *
+ *  Enter a number: 60                                             *
+ *  Enter a number: 38.3                                           *
+ *  Enter a number: 4.89                                           *
+ *  Enter a number: 100.62                                         *
+ *  Enter a number: 75.2295                                        *
+ *  Enter a number: 0                                              *
+ *  The largest number entered was 100.62                          *
+ *                                                                 *
+ *  The input is not restricted to integers.                       *
+ *******************************************************************/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+   
     float input,max;
     
     printf("Enter a number:");
