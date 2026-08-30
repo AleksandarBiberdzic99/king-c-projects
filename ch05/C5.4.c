32690839b9ec485e765352aafbfc82b7975b7eaf
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-30 21:28:11 +0200

    Add problem statement comment to C5.4

--- ch05/C5.4.c
+++ ch05/C5.4.c
@@ -5,6 +5,21 @@
 //  Created by Aleksandar on 6/5/25.
 //
 
+/*******************************************************************
+ *  Chapter 5, Project 4                                           *
+ *                                                                 *
+ *  Reads a wind speed in knots and prints the matching            *
+ *  description from a simplified Beaufort scale:                  *
+ *                                                                 *
+ *      < 1      Calm                                              *
+ *      1-3      Light air                                         *
+ *      4-27     Breeze                                            *
+ *      28-47    Gale                                              *
+ *      48-63    Storm                                             *
+ *      > 63     Hurricane                                         *
+ *******************************************************************/
+
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
