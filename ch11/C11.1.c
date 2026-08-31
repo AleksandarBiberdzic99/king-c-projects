49022ab635a2298ea1de5ff5984d41d60549d493
AleksandarBiberdzic99 <ydwmg6dtby@privaterelay.appleid.com>
2026-08-31 15:37:34 +0200

    Update C11.1.c

--- ch11/C11.1.c
+++ ch11/C11.1.c
@@ -5,6 +5,20 @@
 //  Created by Aleksandar on 30. 7. 2026..
 //
 
+/*******************************************************************
+ *  Chapter 11, Project 1                                          *
+ *                                                                 *
+ *  Reworks Chapter 2, Project 7 around a function that returns    *
+ *  its results through pointers:                                  *
+ *                                                                 *
+ *      void pay_amount(int dollars, int *twenties, int *tens,     *
+ *                      int *fives, int *ones);                    *
+ *                                                                 *
+ *  Given an amount in dollars, it works out the fewest $20, $10,  *
+ *  $5 and $1 bills needed to pay it and stores each count in the  *
+ *  variable the corresponding pointer refers to.                  *
+ *******************************************************************/
+
 #include <stdlib.h>
 #include <stdio.h>
 
